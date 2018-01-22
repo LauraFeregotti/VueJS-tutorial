c5de117dd234f835dd563a70af54f01f6e6ff47f
Laura <laura.feregotti@gmail.com>
2018-01-22 18:38:06 -0300

    finished tutorial number 16

--- app.js
+++ app.js
@@ -1,25 +1,19 @@
-Vue.component('greeting',{
-
-template:'<p>Hey, I am {{name}}. <button v-on:click="changename"> change name</button></p>',
-data:function(){
-return{
-    name: 'Yoshi'
-}
-},
-methods:{
-    changename:function(){
-        this.name='Mario';
-    }
-}
-
+new Vue({
+    el:'#vue-app',
+    data:{
+        output:'your favourite food'
+    },
+        methods:{
+            readRefs:function(){
+                this.$refs
+                this.output= this.$refs.input.value;
+            }
+           
+         }    
+    
 });
 
-new Vue({
-   el:'#vue-app-one'
 
-});
 
-new Vue({
-    el:'#vue-app-two'
- 
- });
+
+
